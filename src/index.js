cb5636e192f4b2eaa2ea70820966804e06507e54
YanYuan <1025138284@qq.com>
2018-06-02 22:53:19 +0800

    build

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd-mobile/dist/antd-mobile.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import FastClick from 'fastclick';
 
 FastClick.attach(document.body);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
